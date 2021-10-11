4485e188f4b2c692ea362c01512ae0e7ab11ff71
slaxeea <kappelers77@gmail.com>
2021-10-11 23:35:12 +0200

    fixes? idk, im too tired for commit msgs

--- scripts/script.js
+++ scripts/script.js
@@ -124,13 +124,42 @@ function loadImages(query) {
       images = otter.imgs;
       $.each(images, function (index, img) {
         divstring = ` 
-      <div class="col-md-4 col otter-index">
+      <div class="col-md-4 col otter-index otter-imgs">
       <div class="card h-200 w-175" >
         <img src="${img}" id="card-img-top" class="card-img-top" alt="otter pic">         
       </div>
     </div>`;
         $("#ankor").append(divstring);
       });
+    }
+  });
+}
+
+function loadCategoryOtters(category) {
+  empty();
+  otters = JSON.parse(localStorage.getItem("data"));
+  $.each(otters, function (index, otter) {
+    tags = otter.tags;
+    divstring = `  
+    <div class="col-lg-6 col">
+      <div class="card card-in-col h-100 justify-content-center otter-library" >
+        <img src="${otter.imgs[0]}" class="card-img-top" alt="otter pic">
+          <div class="card-body">
+            <h5 class="card-title">${otter.common}</h5>
+            <p class="card-text">${otter.desc}</p>
+          </div>              
+          <div class="card-footer">
+            <div class="hstack text-center gap-3">
+              <a href="${otter.link}"  class="btn">Wikipedia</a>
+              <button class="btn" id=${index} onclick="redirect($(this).attr('id'))">Details</Button>
+            </div>
+          </div>
+        </div>
+    </div> `;
+    if (
+      tags.includes(category)
+    ) {
+      $(`#${category}-ankor`).append(divstring);
     }
   });
 }
